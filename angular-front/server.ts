96951fb12a92c3b0a1638b5cf83a6dc446999001
Esper_lvl4 <inventoris7@gmail.com>
2021-11-02 18:54:09 +0300

    - testing current working directory when running an app with pm2

--- angular-front/server.ts
+++ angular-front/server.ts
@@ -12,6 +12,7 @@ import { existsSync } from 'fs';
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/ghost-chess/browser');
+  console.log('Current working directory: ', process.cwd());
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
